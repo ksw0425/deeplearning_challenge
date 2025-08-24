9a277d7c687df26b65948a81fea459432dd014e3
swkim9255 <ksw20011@ajou.ac.kr>
2025-08-25 02:13:59 +0900

    Update qwen25_vl_qlora_finetune_projector.py

--- qwen25_vl_qlora_finetune_projector.py
+++ qwen25_vl_qlora_finetune_projector.py
@@ -507,7 +507,7 @@ def train(
                      gradient_accumulation_steps=16, learning_rate=8e-5, warmup_ratio=0.05,
                      logging_steps=50, eval_steps=100, save_steps=100,  weight_decay=0.0),
         "base": dict(num_train_epochs=10, per_device_train_batch_size=1, per_device_eval_batch_size=1,
-                     gradient_accumulation_steps=16, learning_rate=8e-5, warmup_ratio=0.05,
+                     gradient_accumulation_steps=16, learning_rate=5e-4, warmup_ratio=0.05,
                      logging_steps=50, eval_steps=500, save_steps=500,  weight_decay=0.0),
     }
     p = profiles.get(profile, profiles["base"])
